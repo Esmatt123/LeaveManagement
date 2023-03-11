b0beccfa5334632c38d5567e2cea22d0ec0f2ff5
Esmatt123 <esmatt98@hotmail.com>
2023-03-11 20:50:40 +0100

    Add project files.

---
+++ Configurations/MapperConfig.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using LeaveManagement.Web.Models;
+using LeaveManagement.Web.Data;
+namespace LeaveManagement.Web.Configurations
+{
+    public class MapperConfig : Profile
+    {
+        public MapperConfig() 
+        {
+            CreateMap<LeaveType, LeaveTypeVM>().ReverseMap(); //This states that it is legal to convert from leavetype to leavetypevm and reverse
+        }
+    }
+}
